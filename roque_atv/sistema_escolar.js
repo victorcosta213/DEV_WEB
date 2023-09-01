c2939ba09889151ceabf0c9e5b5e802d741acbaf
victor costa <victormedeirosvcm@gmail.com>
2023-09-01 09:25:39 -0300

    update

--- roque_atv/sistema_escolar.js
+++ roque_atv/sistema_escolar.js
@@ -1,4 +1,5 @@
 const alunos= []
+const nota= []
 const media= []
 const redimento= []
 
@@ -6,26 +7,34 @@ const redimento= []
 //menu
 var i=1
 while(i>0){
-    var nome= prompt("Digite o nome do aluno(a): ")
-    alunos.push(nome)
-    var nota1= prompt("Digite a nota1 do aluno(a): ")
-    var nota2= prompt("Digite a nota2 do aluno(a): ")
-    var nota3= prompt("Digite a nota3 do aluno(a): ")
-    var a = m(nota1,nota2,nota3)
-    media.push(a)
-               switch(Math.round(a)){
-                    case a>=7:
-                        redimento.push("Aprovado")
-                        break;
-                    case a<7 && a>=5:
-                        redimento.push("Recuperação")
-                        break;
-                    case a<5:
-                        redimento.push("Reprovado")
-                        break;
-                    default:
-                        redimento.push("nspu")
-               }
+    console.log("1- Cadastrar Aluno(a)")
+    console.log("2- Cadastrar Notas")
+    console.log("3- Exibir boletim")
+    console.log("4- Sair")
+    let op = prompt("Digite uma opção: ")
+            switch(op){
+                case 1:
+                    var nome= prompt("Digite o nome do aluno(a): ");
+                    alunos.push(nome);
+                    break;
+                case 2:
+                    let indentificar= prompt("Digite o nome do aluno");
+                    let pos= Array.prototype.indexOf(alunos.find((Element)=> Element = indentificar));
+                    var nota1= prompt("Digite a nota1 do aluno(a): ");
+                    var nota2= prompt("Digite a nota2 do aluno(a): ");
+                    var nota3= prompt("Digite a nota3 do aluno(a): ");
+                    nota.splice(pos,nota1,nota2,nota3);
+                    m(nota1,nota2,nota3);
+                    media.splice(pos,m);
+                    break;
+                case 3:
+                    let indentificador= prompt("Digite o nome do aluno");
+                    let posi= Array.prototype.indexOf(alunos.find((Element)=>Element = indentificador));
+                    console.log(`O aluno ${indentificador}`);
+                    console.log(`Ficou com as notas ${nota[posi]}`);
+                    console.log(`Ficou com a media ${media[posi]}`);
+                    break;}
+  
     i= prompt("Deseja cadastrar outro aluno(a):(1-sim/0-não) ")
     console.log("----------------------------------------")
     console.log()
