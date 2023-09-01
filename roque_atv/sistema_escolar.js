4947b59b1df6c8cfca4573f008839e8f8a2ffeb5
victor costa <victormedeirosvcm@gmail.com>
2023-09-01 10:14:29 -0300

    update

--- roque_atv/sistema_escolar.js
+++ roque_atv/sistema_escolar.js
@@ -11,42 +11,46 @@ while(i>0){
     console.log("2- Cadastrar Notas")
     console.log("3- Exibir boletim")
     console.log("4- Sair")
-    let op = prompt("Digite uma opção: ")
+    let op = parseInt(prompt("Digite uma opção: "))
             switch(op){
                 case 1:
                     var nome= prompt("Digite o nome do aluno(a): ");
                     alunos.push(nome);
                     break;
                 case 2:
-                    let indentificar= prompt("Digite o nome do aluno");
-                    let pos= Array.prototype.indexOf(alunos.find((Element)=> Element = indentificar));
-                    var nota1= prompt("Digite a nota1 do aluno(a): ");
-                    var nota2= prompt("Digite a nota2 do aluno(a): ");
-                    var nota3= prompt("Digite a nota3 do aluno(a): ");
-                    nota.splice(pos,nota1,nota2,nota3);
-                    m(nota1,nota2,nota3);
-                    media.splice(pos,m);
+                    let indentificar= prompt("Digite o nome do aluno: ");
+                    let pos= alunos.indexOf(indentificar);
+                    var nota1= parseFloat (prompt("Digite a nota1 do aluno(a): "));
+                    var nota2= parseFloat (prompt("Digite a nota2 do aluno(a): "));
+                    var nota3= parseFloat(prompt("Digite a nota3 do aluno(a): "));
+                    nota.push([nota1,nota2,nota3])
+                    let valor= m(nota1,nota2,nota3);
+                    media.push(valor);
                     break;
                 case 3:
-                    let indentificador= prompt("Digite o nome do aluno");
-                    let posi= Array.prototype.indexOf(alunos.find((Element)=>Element = indentificador));
+                    let indentificador= prompt("Digite o nome do aluno: ");
+                    let posi= alunos.indexOf(indentificador)
                     console.log(`O aluno ${indentificador}`);
                     console.log(`Ficou com as notas ${nota[posi]}`);
-                    console.log(`Ficou com a media ${media[posi]}`);
-                    break;}
+                    console.log(`Ficou com a media ${media[posi].toFixed(2)}`);
+                            if(media[posi]>7){
+                                console.log("Aprovado")
+                            }else if(media[posi]<7 && media>=5){
+                                console.log("recuperação")
+                            }else if(media[posi]<5){
+                                console.log("Reprovado")
+                            }else{
+                                console.log("nspc")
+                            }
+                            break;
+                default: 
+                    console.log("Opção invalida");}
   
-    i= prompt("Deseja cadastrar outro aluno(a):(1-sim/0-não) ")
+    i= prompt("Deseja voltar ao menu (1-sim / 0-não): ")
     console.log("----------------------------------------")
     console.log()
 }
 
-for(var i =0; i< alunos.length;i++){
-   console.log("----------------------------------------")
-   console.log(`O aluno(a): ${nome[i]}`)
-   console.log(`média: ${media[i]}`)
-   console.log(`Situação: ${rendimento[i]}`)
-   console.log("----------------------------------------")
-}
 
 function m(a,b,c){
     var m = (a+b+c)/3
